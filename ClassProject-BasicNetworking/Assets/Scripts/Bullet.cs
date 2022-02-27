fdee9a63bc441211c5bc432e68d953205979d504
Christopher Pollati <christopher_pollati@bloomfield.edu>
2022-02-27 14:10:39 -0500

    Step 10 - Player now gets points for killing enemies

--- ClassProject-BasicNetworking/Assets/Scripts/Bullet.cs
+++ ClassProject-BasicNetworking/Assets/Scripts/Bullet.cs
@@ -5,11 +5,18 @@ public class Bullet : NetworkBehaviour {
 	[SyncVar]
 	public Color color;
 
+	[SyncVar]
+	public uint parentNetId;
+
 	public override void OnStartClient() {
 		gameObject.GetComponent<Renderer>().material.color = color;
 	}
-	
-	void OnTriggerEnter(Collider other) {
-		Destroy(other.gameObject);
-	}
+
+    void OnTriggerEnter(Collider other) {
+        if(isServer) {
+            Player player = NetworkIdentity.spawned[parentNetId].GetComponent<Player>();
+            player.score += 100;
+            Destroy(other.gameObject);
+        }
+    }
 }
--- ClassProject-BasicNetworking/Assets/Scripts/Player.cs
+++ ClassProject-BasicNetworking/Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using Mirror;
 public class Player : NetworkBehaviour {
     [SyncVar]
     public Color color;
+
+	[SyncVar]
+	public int score;
 	
 	float moveSpeed = 1.875f;
 	public GameObject bulletPrefab;
@@ -48,7 +51,8 @@ public class Player : NetworkBehaviour {
 		GameObject bullet = (GameObject)Instantiate(bulletPrefab, this.transform.position + this.transform.right, Quaternion.identity);
 		bullet.GetComponent<Rigidbody>().velocity = Vector3.forward * 17.5f;
 		bullet.GetComponent<Bullet>().color = color;
+		bullet.GetComponent<Bullet>().parentNetId = this.netId;
 		Destroy(bullet,0.875f);
 		NetworkServer.Spawn(bullet);
-	}	
+	}
 }
